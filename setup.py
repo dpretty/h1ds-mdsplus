a2a0142907e6a51048c0f533d5cf9d765e368dea
Dave Pretty <david.pretty@anu.edu.au>
2011-04-15 15:05:08 +1000

    fix setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ execfile('h1ds_mdsplus/version.py')
 
 setup(name='h1ds_mdsplus',
       version=__version__,
-      packages=['h1ds_mdsplus'],
+      packages=['h1ds_mdsplus','h1ds_mdsplus.migrations'],
       package_data={'h1ds_mdsplus':['templates/*/*.html', 'run_event_server']}
       )
