38881c9bee5cdc11c7878fdbaf58e2215aef7f0b
Dave Pretty <david.pretty@gmail.com>
2011-06-27 19:57:39 +1000

    fix template tags for django 1.3

--- h1ds_mdsplus/templatetags/mdsdata.py
+++ h1ds_mdsplus/templatetags/mdsdata.py
@@ -56,88 +56,56 @@ active_filter_html ="""\
 </div>
 """
 
-class H1DSFilterNode(template.Node):
-    def __init__(self, filter_name, is_active=False, fid=None, filter_data=None):
-        """
-        Arguments:
-        filter_name -- name of filter in template
-
-        Keyword arguments:
-        is_active -- set to True if this is an active filter (default: False)
-        filter_data -- name of template variable containing filter parameters, used only if if_active=True to prefill form with currently used filter parameters.
-        """
-        self.filter_var = template.Variable(filter_name)
-        self.is_active = is_active
-        #self.filter_data_var = template.Variable(filter_data)
-        self.filter_data_str = filter_data
-        self.fid_str = fid
-        
-    def render(self, context):
-        try:
-            filter_instance = self.filter_var.resolve(context)
-            request = context['request']
-            existing_query_string = ''.join(['<input type="hidden" name="%s" value="%s" />' %(k,v) for k,v in request.GET.items()])
-            if self.is_active:
-                if self.filter_data_str != None:
-                    filter_data_var = template.Variable(self.filter_data_str)
-                    filter_data = filter_data_var.resolve(context)
-                else:
-                    filter_data = ""
-                filter_id_var = template.Variable(self.fid_str)
-                filter_id = filter_id_var.resolve(context)
-                update_url = reverse("update-filter")
-                remove_url = reverse("remove-filter")
-
-                split_args = filter_data.split('_')
-                input_str = ''.join(['<input title="%(name)s" type="text" size=5 name="%(name)s" value="%(value)s">' %{'name':j,'value':split_args[i]} for i,j in enumerate(filter_instance.template_info['args'])])
-
-                return_string = active_filter_html %{
-                    'update_url':update_url,
-                    'text':filter_instance.template_info['text'],
-                    'input_str':input_str,
-                    'clsname':filter_instance.__name__,
-                    'path':request.path,
-                    'input_query':existing_query_string,
-                    'fid':filter_id,
-                    'remove_url':remove_url,
-                    'existing_query':existing_query_string,
-                    }
-                return return_string
-
-            else:
-                submit_url = reverse("apply-filter")
-                input_str = ''.join(['<input title="%(name)s" type="text" size=5 name="%(name)s">' %{'name':j} for i,j in enumerate(filter_instance.template_info['args'])])
-
-                return_string =  filter_html %{'text':filter_instance.template_info['text'],
-                                               'input_str':input_str,
-                                               'clsname':filter_instance.__name__,
-                                               'submit_url':submit_url,
-                                               'path':request.path,
-                                               'input_query':existing_query_string}
-                return return_string
-        except template.VariableDoesNotExist:
-            #return ''
-            raise
-
-def show_filter(parser, token):
+def get_filter(context, filter_instance, is_active=False, fid=None, filter_data=None):
     try:
-        # split_contents() knows not to split quoted strings.
-        tag_name, filter_name = token.split_contents()
-    except ValueError:
-        raise template.TemplateSyntaxError, "%r tag requires a single argument" % token.contents.split()[0]
-    return H1DSFilterNode(filter_name)
+        request = context['request']
+        existing_query_string = ''.join(['<input type="hidden" name="%s" value="%s" />' %(k,v) for k,v in request.GET.items()])
+        if is_active:
+            if filter_data == None:
+                filter_data = ""
+            update_url = reverse("update-filter")
+            remove_url = reverse("remove-filter")
 
-def show_active_filter(parser, token):
-    try:
-        # split_contents() knows not to split quoted strings.
-        tag_name, filter_name, fid, filter_data = token.split_contents()
-    except ValueError:
-        raise template.TemplateSyntaxError, "%r tag requires a 3 arguments" % token.contents.split()[0]
-    return H1DSFilterNode(filter_name, fid=fid, is_active=True, filter_data=filter_data)
+            split_args = filter_data.split('_')
+            input_str = ''.join(['<input title="%(name)s" type="text" size=5 name="%(name)s" value="%(value)s">' %{'name':j,'value':split_args[i]} for i,j in enumerate(filter_instance.template_info['args'])])
+
+            return_string = active_filter_html %{
+                'update_url':update_url,
+                'text':filter_instance.template_info['text'],
+                'input_str':input_str,
+                'clsname':filter_instance.__name__,
+                'path':request.path,
+                'input_query':existing_query_string,
+                'fid':fid,
+                'remove_url':remove_url,
+                'existing_query':existing_query_string,
+                }
+            return return_string
+
+        else:
+            submit_url = reverse("apply-filter")
+            input_str = ''.join(['<input title="%(name)s" type="text" size=5 name="%(name)s">' %{'name':j} for i,j in enumerate(filter_instance.template_info['args'])])
+
+            return_string =  filter_html %{'text':filter_instance.template_info['text'],
+                                           'input_str':input_str,
+                                           'clsname':filter_instance.__name__,
+                                           'submit_url':submit_url,
+                                           'path':request.path,
+                                           'input_query':existing_query_string}
+            return return_string
+    except template.VariableDoesNotExist:
+        #return ''
+        raise
+
+@register.simple_tag(takes_context=True)
+def show_filters(context, mdsnode):
+    return "".join([get_filter(context, f) for f in mdsnode.available_filters])
 
 
-register.tag('show_filter', show_filter)
-register.tag('show_active_filter', show_active_filter)
+@register.simple_tag(takes_context=True)
+def show_active_filters(context, mdsnode):
+    return "".join([get_filter(context, f, is_active=True, fid=fid, filter_data=fdata) for fid, f, fdata in mdsnode.filter_history])
+
 
 
 class H1DSViewNode(template.Node):
