02a161f0ce99731fb2409e32f18d2774d4ac4b1c
David Pretty <david.pretty@anu.edu.au>
2011-11-23 22:36:40 +1100

    remove test code referring to h1 hard coded path. refs #1

--- h1ds_mdsplus/tests.py
+++ h1ds_mdsplus/tests.py
@@ -15,44 +15,50 @@ def get_nodes(node):
     return output_nodes
 
 
-class NodeUnitTest(unittest.TestCase):
-    """Check that each node in the most recent shot can return HTML."""
+########################################################################
+## disabled test as it has a hardlink to /home/dave/data.             ##
+########################################################################
 
-    def setUp(self):
-        self.tree = MDSPlusTree.objects.create(name="h1data", description="test data", path="/home/dave/data", display_order=10)
-        self.client = Client()
-
-    def tearDown(self):
-        django_mds_tree = MDSPlusTree.objects.all()[0]
-        django_mds_tree.delete()
-
-    def test_html(self):
-        # get root node of default tree, latest shot.
-        for shot in [0]:
-            django_mds_tree = MDSPlusTree.objects.all()[0]
-            mds_tree = django_mds_tree.get_tree(0)
-            root_node = mds_tree.getNode("\\%s::top" %django_mds_tree.name)
-            node_list = get_nodes(root_node)
-            for node_i, node in enumerate(node_list):
-                print node_i+1, len(node_list)
-                print node.getFullPath()
-                print '--'
-                response = self.client.get(mds_to_url(node))
-                self.assertEqual(response.status_code, 200)
-
-    def test_json(self):
-        # get root node of default tree, latest shot.
-        for shot in [0]:
-            django_mds_tree = MDSPlusTree.objects.all()[0]
-            mds_tree = django_mds_tree.get_tree(0)
-            root_node = mds_tree.getNode("\\%s::top" %django_mds_tree.name)
-            node_list = get_nodes(root_node)
-            for node_i, node in enumerate(node_list):
-                print node_i+1, len(node_list)
-                print node
-                print node.getFullPath()
-                url = mds_to_url(node) + '?view=json'
-                print url
-                print '--'
-                response = self.client.get(url)
-                self.assertEqual(response.status_code, 200)
+#
+#class NodeUnitTest(unittest.TestCase):
+#    """Check that each node in the most recent shot can return HTML."""
+#
+#    def setUp(self):
+#        self.tree = MDSPlusTree.objects.create(name="h1data", description="test data", path="/home/dave/data", displ#ay_order=10)
+#        self.client = Client()
+#
+#    def tearDown(self):
+#        django_mds_tree = MDSPlusTree.objects.all()[0]
+#        django_mds_tree.delete()
+#
+#    def test_html(self):
+#        # get root node of default tree, latest shot.
+#        for shot in [0]:
+#            django_mds_tree = MDSPlusTree.objects.all()[0]
+#            mds_tree = django_mds_tree.get_tree(0)
+#            root_node = mds_tree.getNode("\\%s::top" %django_mds_tree.name)
+#            node_list = get_nodes(root_node)
+#            for node_i, node in enumerate(node_list):
+#                print node_i+1, len(node_list)
+#                print node.getFullPath()
+#                print '--'
+#                response = self.client.get(mds_to_url(node))
+#                self.assertEqual(response.status_code, 200)
+#
+#    def test_json(self):
+#        # get root node of default tree, latest shot.
+#        for shot in [0]:
+#            django_mds_tree = MDSPlusTree.objects.all()[0]
+#            mds_tree = django_mds_tree.get_tree(0)
+#            root_node = mds_tree.getNode("\\%s::top" %django_mds_tree.name)
+#            node_list = get_nodes(root_node)
+#            for node_i, node in enumerate(node_list):
+#                print node_i+1, len(node_list)
+#                print node
+#                print node.getFullPath()
+#                url = mds_to_url(node) + '?view=json'
+#                print url
+#                print '--'
+#                response = self.client.get(url)
+#                self.assertEqual(response.status_code, 200)
+#
