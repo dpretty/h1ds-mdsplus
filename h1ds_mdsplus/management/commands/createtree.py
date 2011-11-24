639a3a7dc13b6b0384be1201ea012afc4bb9d35d
David Pretty <david.pretty@anu.edu.au>
2011-11-24 22:50:43 +1100

    django management command to create test mds tree

---
+++ h1ds_mdsplus/management/commands/createtree.py
@@ -0,0 +1,38 @@
+from django.core.management.base import BaseCommand
+import os, sys
+import MDSplus
+import numpy
+
+def create_tree(tree_path):
+    if not os.path.exists(tree_path):
+        os.mkdir(tree_path)
+    os.environ['test_path'] = tree_path
+    for shot_number in xrange(1, 4):
+        t = MDSplus.Tree('test', shot_number, mode="NEW")
+        t.addNode('node_A')
+        t.addNode('node_B')
+
+        node_a = t.getNode('node_A')
+        node_a.setUsage("SIGNAL")
+        node_a.addTag("tag_A")
+
+        node_a.addNode('node_AA')
+        node_a.addNode('node_AB')
+
+        sig = MDSplus.Signal(MDSplus.makeArray(numpy.random.poisson(lam=10, size=1024)), 
+                             None, MDSplus.makeArray(0.1*numpy.arange(1024)))
+        node_a.putData(sig)
+
+        node_aa = t.getNode('\\test::top.node_A.node_AA')
+        node_aa.addTag("tag_AA")
+        node_aa.addNode("node_AAA")
+
+        t.setCurrent('test', shot_number)
+        t.write()
+
+class Command(BaseCommand):
+    args = '<treepath>'
+    help = 'Creates a test mds tree at trepath'
+
+    def handle(self, *args, **options):
+        create_tree(args[0])
